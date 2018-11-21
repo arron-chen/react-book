a63e2e2da71f2f2fc88d1a4b4c3ee3221867f3c3
chentao <1161735119@qq.com>
2018-11-22 00:09:00 +0800

    顶部栏样式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import {
-    HeaderWrapper
+    HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addtion, Button
 } from "./style";
 
 class Header extends Component {
     render() {
         return (
-            <HeaderWrapper>header</HeaderWrapper>
+            <HeaderWrapper>
+                <Logo href="/"></Logo>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载APP</NavItem>
+                    <NavItem className="right">登陆</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addtion>
+                    <Button className="writing">写文章</Button>
+                    <Button className="reg">注册</Button>
+                </Addtion>
+            </HeaderWrapper>
         )
 
     }
